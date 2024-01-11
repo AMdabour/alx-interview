4504c6e1204a6b1c771049561ade302d4fc0feb4
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-11 21:36:42 +0000

    more editing

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """checking if all locked boxes can be opened or not"""
 
 
@@ -6,19 +6,17 @@ def canUnlockAll(boxes):
     """returns true if openable or false if not"""
     if not boxes or not boxes[0]:
         return False
-    
+
     n = len(boxes)
-    visited =[False] * n
+    visited = [False] * n
     visited[0] = True
     stack = [0]
 
     while stack:
         current_box = stack.pop()
         for key in boxes[current_box]:
-            if 0 <= key > n and not visited[key]:
+            if 0 <= key < n and not visited[key]:
                 visited[key] = True
                 stack.append(key)
 
-    if all(visited):
-        return True
-    return False
+    return all(visited)
