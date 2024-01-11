bb8987e733c408494e9adee3869e4d6013b52ab3
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-11 21:04:01 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""checking if all locked boxes can be opened or not"""
+
+
+def canUnlockAll(boxes):
+    """returns true if openable or false if not"""
+    acc = 0
+
+    for i in range(1, len(boxes)):
+        for j in range(0, len(boxes)):
+            if i in boxes[j] and i != j:
+                acc += 1
+                break
+
+    if acc == (len(boxes) - 1):
+        return True
+    return False
