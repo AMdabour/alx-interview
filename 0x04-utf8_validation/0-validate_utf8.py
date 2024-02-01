ce8af52adeff81d1a438847e125654777491765a
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-02-01 19:54:16 +0000

    editing

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,28 +5,25 @@
 def validUTF8(data):
     """UTF-8 validation"""
     # Variable to keep track of remaining bytes for a multi-byte character
-    remaining_bytes = 0
+    num_bytes = 0
 
     for byte in data:
-        # Check if the byte is a continuation byte (starts with '10')
-        if remaining_bytes > 0 and (byte >> 6) == 0b10:
-            remaining_bytes -= 1
-        elif (byte >> 7) == 0:
-            # Single-byte character (starts with '0')
-            remaining_bytes = 0
-        elif (byte >> 5) == 0b110:
-            # Two-byte character (starts with '110')
-            remaining_bytes = 1
-        elif (byte >> 4) == 0b1110:
-            # Three-byte character (starts with '1110')
-            remaining_bytes = 2
-        elif (byte >> 3) == 0b11110:
-            # Four-byte character (starts with '11110')
-            remaining_bytes = 3
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7) != 0:
+                # Invalid starting byte
+                return False
         else:
-            # Invalid starting byte
-            return False
+            # Check if the byte is a continuation byte
+            if (byte >> 6) != 0b10:
+                # Invalid continuation byte
+                return False
+            num_bytes -= 1
 
-    # Check if there are remaining bytes after
-    # iterating through the entire data
-    return remaining_bytes == 0
+    # Check if there are any remaining bytes
+    return num_bytes == 0
